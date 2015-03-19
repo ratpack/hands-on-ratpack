5ae7d5cc9f8b577298557032222a11bfaeb448eb
rhart <russellxhart@googlemail.com>
2015-03-19 21:20:22 +0000

    Simplify register code

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,13 +1,9 @@
-import ratpack.registry.RegistrySpec
-
 import static ratpack.groovy.Groovy.ratpack
+import static ratpack.registry.Registries.just
 
 ratpack {
   handlers {
-    // For this use case there are better options than this.  We will see in future labs
-    register { RegistrySpec spec ->
-      spec.add(DefaultPersonService.instance)
-    }
+    register just(DefaultPersonService.instance)
 
     prefix("person/:id") {
       get("name") {
