17370fe7f58e2baa04f359693dd1db82d7dc25ce
rhart <russellxhart@googlemail.com>
2015-03-23 22:19:48 +0000

    Initial commit of Lab 05

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,5 +1,4 @@
 import static ratpack.groovy.Groovy.ratpack
-import static ratpack.registry.Registries.just
 
 ratpack {
   bindings {
@@ -8,25 +7,6 @@ ratpack {
   }
 
   handlers {
-    prefix("person/:id") {
-      handler { PersonService personService ->
-        long id = allPathTokens.asLong("id")
-        Person p = personService.getPerson(id)
 
-        if (p == null) {
-          response.status(404).send()
-        } else {
-          next(just(p))
-        }
-      }
-
-      get("name") {
-        response.send context.get(Person).name
-      }
-
-      get("status") { Person p ->
-        response.send p.status
-      }
-    }
   }
 }
---
+++ src/test/groovy/RenderSpec.groovy
@@ -0,0 +1,88 @@
+import groovy.json.JsonSlurper
+import ratpack.groovy.test.GroovyRatpackMainApplicationUnderTest
+import ratpack.test.ApplicationUnderTest
+import ratpack.test.http.TestHttpClient
+import spock.lang.Shared
+import spock.lang.Specification
+
+class RenderSpec extends Specification {
+  @Shared ApplicationUnderTest appUnderTest = new GroovyRatpackMainApplicationUnderTest()
+  @Delegate TestHttpClient testClient = appUnderTest.httpClient
+
+  def "01 - can render a String"() {
+    expect:
+    getText() == "Hello Greach!"
+
+    /*
+    Hint:
+    Take a look at `ratpack.handling.Context#render(object)`
+    */
+  }
+
+  def "02 - can render a Groovy Markup Template"() {
+    expect:
+    getText("welcome") == "<!DOCTYPE html><html><body><p>Hello Greach!</p></body></html>"
+
+    /*
+    Hint:
+    Take a look at `ratpack.groovy.template.MarkupTemplateModule`
+    */
+  }
+
+  def "03 - can render a Person as Json"() {
+    given:
+    requestSpec { req ->
+      req.body.type("application/json")
+    }
+
+    when:
+    get("api/person/1")
+
+    then:
+    def person = new JsonSlurper().parseText(response.body.text)
+    with(person) {
+      id == 1
+      name == "Frank Sinatra"
+      status == "new"
+    }
+
+    and:
+    response.headers['content-type'] == 'application/json'
+
+    /*
+    Hint:
+    Take a look at `ratpack.groovy.render.GroovyRendererSupport`
+
+    The Jackson module (`ratpack.jackson.Jackson`) has a support for rendering as JSON
+    */
+  }
+
+  def "04 - can render a Person as Xml"() {
+    given:
+    requestSpec { req ->
+      req.headers.set("Accept", "application/xml")
+    }
+
+    when:
+    get("api/person/1")
+
+    then:
+    def person = new XmlSlurper().parseText(response.body.text)
+    with(person) {
+      id == 1
+      name == "Frank Sinatra"
+      status == "new"
+    }
+
+    and:
+    response.headers['content-type'] == 'application/xml'
+
+    /*
+    Hint:
+    Take a look at `ratpack.groovy.Groovy#markupBuilder(contentType, encoding, closure)`
+
+    Different content types can be rendered using the same renderer using `ratpack.groovy.handling.GroovyContext#byContent(closure)`
+    */
+  }
+
+}
