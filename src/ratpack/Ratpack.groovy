fe07b2ef0974d820b402d2ac8602cd8c657bba5f
rhart <russellxhart@googlemail.com>
2015-07-21 17:51:56 +0100

    Update to latest Ratpack

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,30 +1,30 @@
 import ratpack.groovy.template.MarkupTemplateModule
-import ratpack.jackson.JacksonModule
+import ratpack.jackson.guice.JacksonModule
 
 import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
-  bindings {
-    bind BookRepository, DefaultBookRepository
-    bind BookService, DefaultBookService
-    bind BookRenderer
-    add MarkupTemplateModule
-    add JacksonModule
-  }
-
-  handlers {
-    get {
-      render "Hello Greach!"
+    bindings {
+        bind BookRepository, DefaultBookRepository
+        bind BookService, DefaultBookService
+        bind BookRenderer
+        module MarkupTemplateModule
+        module JacksonModule
     }
 
-    get("welcome") {
-      render groovyMarkupTemplate("index.gtpl", welcomeMessage: "Hello Greach!")
-    }
+    handlers {
+        get {
+            render "Hello Greach!"
+        }
 
-    get("api/book/:isbn") { BookService bookService ->
-      String isbn = pathTokens["isbn"]
-      render bookService.getBook(isbn)
+        get("welcome") {
+            render groovyMarkupTemplate("index.gtpl", welcomeMessage: "Hello Greach!")
+        }
+
+        get("api/book/:isbn") { BookService bookService ->
+            String isbn = pathTokens["isbn"]
+            render bookService.getBook(isbn)
+        }
     }
-  }
 }
