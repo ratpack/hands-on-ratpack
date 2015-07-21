a8c58a301d5d2591f37fbe32f2b2e6d60cf0da95
rhart <russellxhart@googlemail.com>
2015-07-21 17:03:33 +0100

    Update to latest Ratpack

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,28 +1,27 @@
 import static ratpack.groovy.Groovy.ratpack
-import static ratpack.registry.Registries.just
 
 ratpack {
-  bindings {
-    bind BookRepository, DefaultBookRepository
-    bind BookService, DefaultBookService
-  }
-
-  handlers {
-    prefix("book/:isbn") {
-      handler { BookService bookService ->
-        String isbn = allPathTokens["isbn"]
-        Book b = bookService.getBook(isbn)
-
-        next(just(b))
-      }
-
-      get("title") { Book b ->
-        response.send b.title
-      }
-
-      get("author") { Book b ->
-        response.send b.author
-      }
+    bindings {
+        bind BookRepository, DefaultBookRepository
+        bind BookService, DefaultBookService
     }
-  }
+
+    handlers {
+        prefix("book/:isbn") {
+            all { BookService bookService ->
+                String isbn = allPathTokens["isbn"]
+                Book b = bookService.getBook(isbn)
+
+                next(single(b))
+            }
+
+            get("title") { Book b ->
+                response.send b.title
+            }
+
+            get("author") { Book b ->
+                response.send b.author
+            }
+        }
+    }
 }
