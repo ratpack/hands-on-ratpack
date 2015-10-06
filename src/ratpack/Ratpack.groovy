dca111578c3f8ae66168ef3f43619ad44f87929b
Danny Hyun <hyun.danny+github@gmail.com>
2015-10-05 23:25:23 -0400

    Update to Ratpack 1.0

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -4,15 +4,17 @@ import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
-    bindings {
-        module MarkupTemplateModule
+  bindings {
+    module MarkupTemplateModule
+  }
+
+  handlers {
+    get {
+      render groovyMarkupTemplate("index.gtpl", title: "My Ratpack App")
     }
 
-    handlers {
-        get {
-            render groovyMarkupTemplate("index.gtpl", title: "My Ratpack App")
-        }
-
-        files { dir "public" }
+    files {
+      dir "public"
     }
+  }
 }
